57c4ac168da999590ba0f722e6919d7f7bbb5b3e
Roman Yarema <romanyarema33@gmail.com>
2025-12-02 00:15:27 +0200

    add information for dmytro

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,6 +10,13 @@ const Body = () => {
           <p style={styles.position}>Project Manager</p>
         </div>
       </div>
+      <div style={styles.card}>
+        <img src={"/avatar-default.png"} alt="Dmytro Kozariuk" style={styles.photo} />
+        <div style={styles.textContainer}>
+          <h2 style={styles.name}>Dmytro Kozariuk</h2>
+          <p style={styles.position}>Developer</p>
+        </div>
+      </div>
     </main>
   );
 };
