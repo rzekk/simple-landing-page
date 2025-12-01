f65ac817f057d5141d5bb74b3763de8100149707
Roman Yarema <romanyarema33@gmail.com>
2025-12-01 19:03:48 +0200

    Added info for Roman

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/avatar-default.png"} alt="Roman Yarema" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Roman Yarema</h2>
+          <p style={styles.position}>Project Manager</p>
         </div>
       </div>
     </main>
